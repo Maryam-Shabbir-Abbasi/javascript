217a5c32532718ef23d3f9fe138050c8d994e4aa
unknown <maryam0shabbir0@gmail.com>
2024-04-24 06:36:27 -0700

    js-ch1:alert

---
+++ chapter.no1/app.js
@@ -0,0 +1,13 @@
+alert("Error! Please enter a valid password.");
+alert("Welcome to JS Land...\nHappy coding");
+alert("Welcome to JS Land");
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+alert("Hello...I can JS through my web browser's console")
+console.log("");
+
+
+
+
+
+
+
