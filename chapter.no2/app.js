cef83936f0ee9914b77362349e8f93624dc768c5
unknown <maryam0shabbir0@gmail.com>
2024-05-04 03:09:17 -0700

    ch2 added

---
+++ chapter.no2/app.js
@@ -0,0 +1,18 @@
+var username = "maryjjuro";
+var myname ="MaryamShabbir";
+var message ="Hello world";
+alert(message);
+var Name="Jhone Doe";
+var age="15 years old";
+var qualification="Certified Mobile Application Deveplopment";
+alert(Name);
+alert(age);
+alert(qualification);
+var food ="PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(food);
+var email="My email address is mryamshabi409@gmail.com";
+alert(email);
+var book="A smarter way to learn JavaScript";
+var Message="I am trying to learn from the Book \n A smarter way to learn javaScript"
+alert(Message);
+document.write("Yah! I can write HTML content through JavaScript");
